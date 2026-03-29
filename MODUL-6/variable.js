f58eb4a07e0e08a2c691193c56c77f555b0d44d3
ajitriprasetyo <ajitriprasetyoo23@gmail.com>
2026-03-29 17:16:51 +0700

    baru

---
+++ MODUL-6/variable.js
@@ -0,0 +1,11 @@
+// Deklarasi variabel menggunakan kata kunci var
+var nama = "Aji Tri Prasetyo"; // Tipe data String
+var nim = "2311102074"; // Tipe data String
+
+// Menampilkan isi variabel ke terminal
+console.log("Halo, nama saya " + nama);
+console.log("NIM saya adalah " + nim);
+
+// Variabel juga bisa menampung angka (Number)
+var umur = 20;
+console.log("Umur: " + umur + " tahun");
